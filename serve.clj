bba6ff6040766895bdb98c729f6349a03b2f1151
Thorsten Wilms <t_w_@freenet.de>
2011-09-16 11:00:56 +0200

    Add serve.clj for starting appengine-magic/Jetty.

---
+++ serve.clj
@@ -0,0 +1,19 @@
+(ns user
+  "Start appengine-magic development mode with Jetty.
+   Paste to REPL to make sure datastore operations will be available from the REPL (they won't after
+   slime-eval-buffer).
+
+   Triggering this script via slime-load-hook does not work, as appengine-magic tries to determine
+   what kind of environment it runs in (see its in-appengine-interactive-mode?), resulting in
+   ae/serve being unavailabel, then."
+  (:require [appengine-magic.core :as ae]
+            [tlog.core])
+  (:use [clojure.stacktrace]
+        [clojure.repl :only [source]]
+        [clojure.contrib.repl-utils :only (show)]))
+
+(compile 'tlog.core)
+(in-ns 'tlog.core)
+(ae/serve tlog-app)
+(println "Interactive Jetty instance started.")
+(intern 'clojure.core '*out* *out*)
